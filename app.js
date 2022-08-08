95bf42dcf291886f251a7a158ae41f0d96d9cb3d
tarekeg <tarek.elghoul@esprit.tn>
2022-08-08 15:54:21 +0100

    Adding well known route

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world Bis 3");
+  res.send("Hello world Bis 4");
 });
 
 
 app.get('/apple-app-site-association', function(request, response) {
+  console.log('trying to download')
+  response.sendFile(__dirname +  '/apple-app-site-association');
+});
+
+app.get('/.well-known/apple-app-site-association', function(request, response) {
   console.log('trying to download')
   response.sendFile(__dirname +  '/apple-app-site-association');
 });
