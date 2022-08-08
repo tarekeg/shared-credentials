a7bffe5465360ede427132d888357db3a3ef3694
tarekeg <tarek.elghoul@esprit.tn>
2022-08-08 15:09:48 +0100

    Adding apple app site file

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world Bis");
+});
+
+
+app.get('/.well-known/apple-app-site-association', function(request, response) {
+  response.sendFile(__dirname +  '/apple-app-site-association');
 });
 
 app.listen(PORT, () => {
