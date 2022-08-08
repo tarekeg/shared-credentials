d6b63ecbbaf67cc56e101a0230558d858b3443f3
tarekeg <tarek.elghoul@esprit.tn>
2022-08-08 15:13:19 +0100

    change dir route

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.get("", (req, res) => {
 });
 
 
-app.get('/.well-known/apple-app-site-association', function(request, response) {
-  response.sendFile(__dirname +  '/apple-app-site-association');
+app.get('/apple-app-site-association', function(request, response) {
+  response.sendFile("/apple-app-site-association");
 });
 
 app.listen(PORT, () => {
